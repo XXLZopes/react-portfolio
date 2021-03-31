95a3db6c19d724a985541c67c9d22867ff4a6161
XXLZopes <afordm99@gmail.com>
2021-03-31 11:29:45 -0700

    fixing resume scroll to

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -58,7 +58,7 @@ return(
                         setResumeSelected(true)
                         setTimeout(() => {
                             window.scrollBy({ top: scrollFunction(1955), left: 0, behavior: 'smooth' });
-                        }, 10);
+                        }, 50);
                     }
                     }
                     >Resume</span>
